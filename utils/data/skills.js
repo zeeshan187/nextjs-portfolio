95ebbd3e9280781a992a549663a92ffdaea4beaf
Zeeshan <mzeeshi187@gmail.com>
2024-09-10 21:43:23 -0700

    initial push

---
+++ utils/data/skills.js
@@ -0,0 +1,24 @@
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'Javascript',
+  'Typescript',
+  'React',
+  'Next JS',
+  'Tailwind',
+  'Bootstrap',
+  'MaterialUI',
+  'Ant Design',
+  'Redux',
+
+  'MongoDB',
+  'MySQL',
+  'PostgreSQL',
+  'Git',
+  'AWS',
+  'Docker',
+  'Go',
+  'Figma',
+  'Strapi'
+]
+
